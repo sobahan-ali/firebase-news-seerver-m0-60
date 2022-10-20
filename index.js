4fbf386fbb281a136bfd3384f4678654af2ab037
sobahan-ali <sobahaan.ali@gmail.com>
2022-10-20 18:54:07 +0600

    new server added

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json')
+
+app.get('/', (req, res) => {
+    res.send('news start')
+})
+
+app.get('/news', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log('app lliestening port is', port);
+})
